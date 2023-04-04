7f0093346b1f0dd86f90b7f6c170288cf52441a6
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-04 20:38:54 -0300

    rota de autenticacao de usuario

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const UserModel = require("../models/User")
 const User = require("../models/User")
+const bcrypt = require("bcryptjs")
 
 const router = express.Router()
 
@@ -17,6 +18,7 @@ router.post("/register", async(req, res) => {
         })
     }
 
+
     const User = await UserModel.create(req.body)
     
     User.password = undefined;//retirar o password na hora do retorno para mostrar par ao usuario
@@ -27,6 +29,40 @@ router.post("/register", async(req, res) => {
         data: User
     })
 })
+
+
+router.post("/autenticate", async(req, res) => {
+   
+    const { cpf, password } = req.body
+
+    //<sempre que utilizar o model do banco é preciso possuir o await
+    const user = await UserModel.findOne({cpf}).select("+password")
+
+    if(!user){
+        return res.status(400).json({
+            error: true,
+            message: "User not found"
+        })
+    }
+    //comparadno a senha recebida com a senha do usuario no banco de dados que ta com o hash
+    //importantissimo a parte do await
+    if(!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({
+            error:true,
+            message: "Invalid password"
+        })
+    }
+
+    user.password = undefined
+
+    return res.json({
+        error: false,
+        message: "Loged!",
+        data: User
+    })
+})
+
+
 
 
 router.delete("/delete", (req, res) => {
