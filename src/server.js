26419312607b681151e6d07f9cfa8af08739d841
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-04 21:47:18 -0300

    adicionando middlware de autenticacao na rota#2

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const app = express()
 const AuthController = require("./controllers/AuthController")
 const AdminController = require("./controllers/AdminController")
 
+const authenticateMiddleware = require("./middlewares/authenticate")
+
 //habilitar o recebimento de json via post
 app.use(express.json())
 
 //portas das requisições
 app.use("/auth", AuthController)
-app.use("/admin", AdminController)
+app.use("/admin", authenticateMiddleware, AdminController)
 
 app.listen(3000, ()=>{
     console.log("Server is running in 3000")
